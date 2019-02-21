90ed879ed067e9082cd541cfff065dab67afdcfa
ajc1023 <47224949+ajc1023@users.noreply.github.com>
2019-02-21 11:16:40 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
